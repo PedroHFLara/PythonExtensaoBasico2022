d34565845ef749a378c7a2154bdba833b8feb8d3
Pedro Henrique Ferreira Lara <103151820+PedroHFLara@users.noreply.github.com>
2022-10-27 00:34:49 +0000

    aula2

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-
-# comando input(): quero permitir que o usuário digite algo...
-nome = input("Digite seu nome: ")
-
-#comando de saída / exibir na tela
-print(f"Boa noite, seu nome é {nome}")
-
-idade = input("Qual a sua idade? ")
-
-print(f"Sua idade é: {idade}")
-
+# NOTA
